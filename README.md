69c5560f6ed747d0a8b041f92262cabaffb5cb1c
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2024-06-03 14:41:56 +0600

    side nav done

--- README.md
+++ README.md
@@ -69,6 +69,104 @@ import {
   </SideContent>
 </Navbar>
 ```
+## Responsive Side Navbar
+<a href="https://eb-responsive-navigation.vercel.app"><img src="/public/sidenav.png" alt="navbar" /></a>
+
+## Usage Code
+
+```javascript
+import {
+    SideNavbar,
+    NavbarBrand,
+    MenuBar,
+    DropDown,
+    NavItem,
+    Contact,
+  } from "responsive-navigation";
+
+  <SideNavbar style={{ width: "250px", background: "white" }}>
+      <NavbarBrand>
+      <a href="/" style={{ fontSize: "30px", fontWeight: "700" }}>
+          LOGO
+      </a>
+      </NavbarBrand>
+      <MenuBar>
+      <NavItem>
+          <a href="/">
+          <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+          >
+              <path fill="none" d="M0 0h24v24H0V0z"></path>
+              <path d="m18.36 9 .6 3H5.04l.6-3h12.72M20 4H4v2h16V4zm0 3H4l-1 5v2h1v6h10v-6h4v6h2v-6h1v-2l-1-5zM6 18v-4h6v4H6z"></path>
+          </svg>
+          <p>Dashbord</p>
+          </a>
+      </NavItem>
+      <NavItem>
+          <a href="/">Notification</a>
+      </NavItem>
+      <DropDown label="Servies">
+          <NavItem>
+          <a href="/">Service 1</a>
+          </NavItem>
+          <DropDown label="Servies">
+          <NavItem>
+              <a href="/">Service 1</a>
+          </NavItem>
+          <NavItem>
+              <a href="/">Service 2</a>
+          </NavItem>
+          <NavItem>
+              <a href="/">Service 3</a>
+          </NavItem>
+          </DropDown>
+          <NavItem>
+          <a href="/">Service 3</a>
+          </NavItem>
+      </DropDown>
+      <NavItem>
+          <a href="/">New Order</a>
+      </NavItem>
+      </MenuBar>
+      <Contact>
+      <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <svg
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 512 512"
+          height="2em"
+          width="2em"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+              fill="none"
+              strokeMiterlimit="10"
+              strokeWidth="32"
+              d="M451 374c-15.88-16-54.34-39.35-73-48.76-24.3-12.24-26.3-13.24-45.4.95-12.74 9.47-21.21 17.93-36.12 14.75s-47.31-21.11-75.68-49.39-47.34-61.62-50.53-76.48 5.41-23.23 14.79-36c13.22-18 12.22-21 .92-45.3-8.81-18.9-32.84-57-48.9-72.8C119.9 44 119.9 47 108.83 51.6A160.15 160.15 0 0 0 83 65.37C67 76 58.12 84.83 51.91 98.1s-9 44.38 23.07 102.64 54.57 88.05 101.14 134.49S258.5 406.64 310.85 436c64.76 36.27 89.6 29.2 102.91 23s22.18-15 32.83-31a159.09 159.09 0 0 0 13.8-25.8C465 391.17 468 391.17 451 374z"
+          ></path>
+          </svg>
+          <p
+          style={{
+              display: "flex",
+              flexDirection: "column",
+              fontSize: "14px",
+          }}
+          >
+          <span> 1900 - 888</span>
+          <span>24/7 Support</span>
+          </p>
+      </div>
+      </Contact>
+  </SideNavbar>
+```
+
 ## Responsive Mega Menu
 <a href="https://eb-responsive-navigation.vercel.app"><img src="/public/mega_menu.png" alt="navbar" /></a>
 
--- src/demo/SidenavLive.jsx
+++ src/demo/SidenavLive.jsx
@@ -1,9 +1,9 @@
-import Contact from "../components/Contact";
-import DropDown from "../components/DropDown";
-import MenuBar from "../components/MenuBar";
-import NavItem from "../components/NavItem";
-import NavbarBrand from "../components/NavbarBrand";
 import SideNavbar from "../components/SideNavbar";
+import NavbarBrand from "../components/NavbarBrand";
+import MenuBar from "../components/MenuBar";
+import DropDown from "../components/DropDown";
+import NavItem from "../components/NavItem";
+import Contact from "../components/Contact";
 import CopyButton from "./CopyButton";
 import markdownit from "markdown-it";
 import hljs from "highlight.js";
@@ -26,58 +26,99 @@ const SidenavLive = () => {
   });
 
   const markdownContainer = md.render(` 
-    \`\`\`javascript
-    import {
-      Navbar,
-      NavbarBrand,
-      NavbarContent,
-      NavItem,
-      SideContent,
-      SideItems,
-      DropDown
-    } from "responsive-navigation";
-    
-    <Navbar
-        style={{
-          backgroundColor: "white",
-          borderBottom: "1px solid #dee2e6",
-          padding: "20px 0",
-        }}
-      >
+  \`\`\`javascript
+  import {
+    SideNavbar,
+    NavbarBrand,
+    MenuBar,
+    DropDown,
+    NavItem,
+    Contact,
+  } from "responsive-navigation";
+
+  <SideNavbar style={{ width: "250px", background: "white" }}>
       <NavbarBrand>
-        <a href="/" style={{ fontSize: "30px", fontWeight: "700" }}>
+      <a href="/" style={{ fontSize: "30px", fontWeight: "700" }}>
           LOGO
-        </a>
+      </a>
       </NavbarBrand>
-      <NavbarContent>
-        <NavItem>
-          <a href="/">Home</a>
-        </NavItem>
-        <NavItem>
-          <a href="/">About</a>
-        </NavItem>
-        <DropDown style={{ width: "150px" }} label="Services">
+      <MenuBar>
+      <NavItem>
+          <a href="/">
+          <svg
+              stroke="currentColor"
+              fill="currentColor"
+              strokeWidth="0"
+              viewBox="0 0 24 24"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+          >
+              <path fill="none" d="M0 0h24v24H0V0z"></path>
+              <path d="m18.36 9 .6 3H5.04l.6-3h12.72M20 4H4v2h16V4zm0 3H4l-1 5v2h1v6h10v-6h4v6h2v-6h1v-2l-1-5zM6 18v-4h6v4H6z"></path>
+          </svg>
+          <p>Dashbord</p>
+          </a>
+      </NavItem>
+      <NavItem>
+          <a href="/">Notification</a>
+      </NavItem>
+      <DropDown label="Servies">
           <NavItem>
-            <a href="/">Service 1</a>
+          <a href="/">Service 1</a>
+          </NavItem>
+          <DropDown label="Servies">
+          <NavItem>
+              <a href="/">Service 1</a>
           </NavItem>
           <NavItem>
-            <a href="/">Service 2</a>
+              <a href="/">Service 2</a>
           </NavItem>
           <NavItem>
-            <a href="/">Service 3</a>
+              <a href="/">Service 3</a>
           </NavItem>
-        </DropDown>
-        <NavItem>
-          <a href="/">Contact</a>
-        </NavItem>
-      </NavbarContent>
-      <SideContent>
-        <SideItems>Sign In</SideItems>
-        <SideItems>Sign Up</SideItems>
-      </SideContent>
-    </Navbar>
-    \`\`\`
-      `);
+          </DropDown>
+          <NavItem>
+          <a href="/">Service 3</a>
+          </NavItem>
+      </DropDown>
+      <NavItem>
+          <a href="/">New Order</a>
+      </NavItem>
+      </MenuBar>
+      <Contact>
+      <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+          <svg
+          stroke="currentColor"
+          fill="currentColor"
+          strokeWidth="0"
+          viewBox="0 0 512 512"
+          height="2em"
+          width="2em"
+          xmlns="http://www.w3.org/2000/svg"
+          >
+          <path
+              fill="none"
+              strokeMiterlimit="10"
+              strokeWidth="32"
+              d="M451 374c-15.88-16-54.34-39.35-73-48.76-24.3-12.24-26.3-13.24-45.4.95-12.74 9.47-21.21 17.93-36.12 14.75s-47.31-21.11-75.68-49.39-47.34-61.62-50.53-76.48 5.41-23.23 14.79-36c13.22-18 12.22-21 .92-45.3-8.81-18.9-32.84-57-48.9-72.8C119.9 44 119.9 47 108.83 51.6A160.15 160.15 0 0 0 83 65.37C67 76 58.12 84.83 51.91 98.1s-9 44.38 23.07 102.64 54.57 88.05 101.14 134.49S258.5 406.64 310.85 436c64.76 36.27 89.6 29.2 102.91 23s22.18-15 32.83-31a159.09 159.09 0 0 0 13.8-25.8C465 391.17 468 391.17 451 374z"
+          ></path>
+          </svg>
+          <p
+          style={{
+              display: "flex",
+              flexDirection: "column",
+              fontSize: "14px",
+          }}
+          >
+          <span> 1900 - 888</span>
+          <span>24/7 Support</span>
+          </p>
+      </div>
+      </Contact>
+  </SideNavbar>
+  \`\`\`
+    `);
   const description = {
     __html: markdownContainer,
   };
